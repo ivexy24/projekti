05f869f3782462f66fc53da44fe5f25b856dc91e
Ivexy <noel.pilic@gmail.com>
2021-04-16 10:01:49 +0200

    addeed fetch

--- math-operation.js
+++ math-operation.js
@@ -1,36 +1,85 @@
 module.exports = function(RED) {
     function mathOperationNode(config) {
         RED.nodes.createNode(this,config);  
-        this.operation = config.operation;
+        this.options = config.options;
         var node = this;
         
         node.on('input', function(msg) {
-            let operation = node.operation;
-
-
-            switch(operation) {
-                case("sum"):{
-                    msg.payload = msg.payload + +config.number;
+            let options = node.options
+            switch(options){
+                case('ZHT0062_2.System.LeadPump.Selection'): {
+                    if(msg.payload.name === options){
+                        msg.payload = msg.payload.value;
+                        msg.topic = "LeadPump";
+                        var globalContext = node.context().global;
+                        globalContext.set("LeadPump", msg.payload)
+                        node.status({
+                            fill:"blue",
+                            shape:"ring",
+                            text:msg.payload
+                        })
+                        node.send(msg)
+                    }
+                    break; 
+                    
                 }
+                case('ZHT0062_2.Pump.1.DPT.Pressure'): {
+                    if(msg.payload.name === options){
+                        msg.payload = msg.payload.value;
+                        msg.topic = "setpoint";
+                        var globalContext = node.context().global;
+                        globalContext.set("Pressure1", msg.payload)
+                        node.status({
+                            fill:"blue",
+                            shape:"ring",
+                            text:msg.payload
+                        })
+                        node.send(msg)
+                    }
                     break;
-                case("sub"): {
-                    msg.payload = msg.payload - +config.number;
                 }
+                case('ZHT0062_1.System.Command.Run'): {
+                    if(msg.payload.name === options){
+                        msg.payload = msg.payload.value;
+                        msg.topic = "SystemRunCmd";
+                        var globalContext = node.context().global;
+                        globalContext.set("SystemRunCmd", msg.payload)
+                        node.status({
+                            fill:"blue",
+                            shape:"ring",
+                            text:msg.payload
+                        })
+                        node.send(msg)
+                    }
                     break;
-                case('div'): {
-                    msg.payload = msg.payload / +config.number;
                 }
-                    break;
-                case('mult'): {
-                    msg.payload = msg.payload * +config.number;
+                // case('one'):{
+                //     if(msg.payload.name === "ZHT0062_2.System.LeadPump.Selection"){
+                //         msg.payload = msg.payload.value;
+                //         msg.topic = "LeadPump";
+                //         node.send(msg)
+                //     }
+                // }
+                // case('two'):{
+                //     if(msg.payload.name === "ZHT0062_2.Pump.1.DPT.Pressure"){
+                //         msg.payload = msg.payload.value;
+                //         msg.topic = "setpoint";
+                        
+                //         node.send(msg)
+                //     }
+                //     break;
                 }
-
-            }
-       
-            node.send(msg);                      
+                // case('three'): {
+                //     if(msg.payload.name === "ZHT0062_1.System.Command.Run"){
+                //         msg.payload = msg.payload.value;
+                //         msg.topic = "SystemRunCmd";
+                //         node.send(msg)
+                //     }
+                //     break;
+                // }
+            
         });
     }
     RED.nodes.registerType("math-operation",mathOperationNode);
     
 }
-
